19b2822f00f83e9bbff908553aafc15f101ad6cd
mtsgi <oq@live.jp>
2019-03-07 22:40:34 +0900

    表記等の微調整

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :apps do
     collection { get "search" }
   end
+  root "top#index"
   get "apps/:id/download" => "apps#download"
   get "apps/:id/delete" => "apps#delete"
-  root "top#index"
 end
